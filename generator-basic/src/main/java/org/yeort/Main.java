96012fc32c4d9c7b4966313278b824d0a3e031db
Lele962 <ysteamlqszl2@163.com>
2024-03-19 18:09:29 +0800

    init

---
+++ generator-basic/src/main/java/org/yeort/Main.java
@@ -0,0 +1,14 @@
+package org.yeort;
+
+/**
+ * @author yeort
+ * */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        for(int i=0;i<5;i++){System.out.println("i = "+ i );}
+
+    }
+}
---
+++ generator-basic/src/main/java/org/yeort/generator/StaticGenerator.java
@@ -0,0 +1,24 @@
+package org.yeort.generator;
+import cn.hutool.core.io.FileUtil;
+
+import java.io.File;
+
+/**
+ * 静态文件生成器
+ * */
+public class StaticGenerator {
+    public static void main(String[] args) {
+        String projectPath = System.getProperty("user.dir");
+        String inputPath = "generator-demo-projects" + File.separator + "acm-template";
+        String outputPath = "";
+    }
+
+    /**
+     * 使用hutool来进行文件复制
+     * @param inputPath 输入路径
+     * @param outputPath 输出路径
+     */
+    public static void copyFilesByHutool(String inputPath, String outputPath){
+        FileUtil.copy(inputPath, outputPath,false);
+    }
+}
